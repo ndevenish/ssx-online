1e308ca99884de89c9843e3948e89b09dd452d6f
Nicholas Devenish <ndevenish@gmail.com>
2022-10-11 19:49:45 +0100

    Rename to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/js/index.js",
+    entry: "./src/js/index",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "build"),
